592a18704b0544379602b8150bb19a0c913db066
Clayton Rafael <clayton.rssouza@gmail.com>
2022-02-27 22:42:30 -0300

    feat: implement photo modal

--- src/components/atoms/feed-photos-item/types.d.ts
+++ src/components/atoms/feed-photos-item/types.d.ts
@@ -2,5 +2,5 @@ import { PhotoProps } from 'types';
 
 interface FeedPhotoItemProps {
   photo: PhotoProps;
-  setModalPhoto: React.Dispatch<React.SetStateAction<PhotoProps | undefined>>;
+  setModalPhoto: React.Dispatch<React.SetStateAction<PhotoProps | null>>;
 }
